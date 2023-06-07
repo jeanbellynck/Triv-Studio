0238aceb7d1d4c7799dab27feed4bbb7c643c6b1
TheDiffi <64838662+TheDiffi@users.noreply.github.com>
2023-06-07 02:09:57 +0200

    Assets + Parallax

---
+++ Assets/Code/Scripts/playerMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed = 20f;
+    private Vector2 motion;
+
+    // Update is called once per frame
+    void Update()
+    {
+        motion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        transform.Translate(motion * speed * Time.deltaTime);
+    }
+}
