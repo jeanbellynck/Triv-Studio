f2c6e71e25d42161eb2af32ef95d02b39737abfc
agaa-aaga <agatakim98@gmail.com>
2023-06-18 23:32:28 +0200

    Implemented logic for spawning end segment with regards to player's traveled distance

--- Assets/Scenes/tests/Basic Movement/simple2dMovement.cs
+++ Assets/Scenes/tests/Basic Movement/simple2dMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
@@ -7,10 +5,18 @@ public class playerMovement : MonoBehaviour
     public float speed = 5f;
     private Vector2 motion;
 
+    /// <summary>
+    /// Returns the traveled distance of the player.
+    /// </summary>
+    public float Distance { get; private set; }
+
     // Update is called once per frame
     void Update()
     {
+        var currentPosition = transform.position;
         motion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         transform.Translate(motion * speed * Time.deltaTime);
+        var newPosition = transform.position;
+        Distance += Vector3.Distance(currentPosition, newPosition);
     }
 }
