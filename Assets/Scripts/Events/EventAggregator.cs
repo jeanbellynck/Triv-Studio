8dbf44f4baf1a68ded41aeabf15d0ee0a4c0fb9b
agaa-aaga <agatakim98@gmail.com>
2023-06-19 13:46:36 +0200

    Added EventAggregator for publish / subscribe

---
+++ Assets/Scripts/Events/EventAggregator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Events
+{
+    /// <summary>
+    /// Defines a service for publishing and subscribing to events.
+    /// </summary>
+    public sealed class EventAggregator
+    {
+        private static readonly Lazy<EventAggregator> Lazy = new(() => new EventAggregator());
+
+        private readonly Dictionary<Type, List<object>> _subscribersByType = new();
+        private readonly object _syncRoot = new();
+
+        /// <summary>
+        /// Provides access to the event aggregator instance.
+        /// </summary>
+        public static EventAggregator Instance => Lazy.Value;
+
+        /// <summary>
+        /// Subscribes to an event.
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>
+        /// <param name="subscriber">The subscriber</param>
+        public void Subscribe<TEvent>(IEventHandler<TEvent> subscriber)
+        {
+            var type = typeof(TEvent);
+
+            lock (_syncRoot)
+            {
+                if (!_subscribersByType.ContainsKey(type))
+                {
+                    _subscribersByType[type] = new List<object>();
+                }
+
+                if (_subscribersByType[type].Contains(subscriber))
+                {
+                    return;
+                }
+
+                _subscribersByType[type].Add(subscriber);
+            }
+        }
+
+        /// <summary>
+        /// Unsubscribes from an event.
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>
+        /// <param name="subscriber">The subscriber.</param>
+        /// <remarks>The subscriber must manually unsubscribe from the event at some point to avoid any memory leaks.
+        /// When used in a MonoBehavior, cleanup can be e.g. done in OnDestroy().</remarks>
+        public void Unsubscribe<TEvent>(IEventHandler<TEvent> subscriber)
+        {
+            var type = typeof(TEvent);
+
+            lock (_syncRoot)
+            {
+                if (!_subscribersByType.TryGetValue(type, out var subscribers))
+                {
+                    return;
+                }
+
+                if (subscribers.Contains(subscriber))
+                {
+                    subscribers.Remove(subscriber);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Publishes an event.
+        /// </summary>
+        /// <typeparam name="TEvent">The event.</typeparam>
+        /// <param name="args"></param>
+        public void Publish<TEvent>(TEvent args)
+        {
+            var type = typeof(TEvent);
+
+            lock (_syncRoot)
+            {
+                if (!_subscribersByType.TryGetValue(type, out var allSubscribers))
+                {
+                    return;
+                }
+
+                var subscribers = allSubscribers.OfType<IEventHandler<TEvent>>();
+
+                foreach (var subscriber in subscribers)
+                {
+                    subscriber.Handle(args);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Events/IEventHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Events
+{
+    /// <summary>
+    /// Responsible for handling an event published by <see cref="EventAggregator"/>.
+    /// </summary>
+    /// <typeparam name="TEvent"></typeparam>
+    public interface IEventHandler<in TEvent>
+    {
+        /// <summary>
+        /// Handles an event.
+        /// </summary>
+        /// <param name="value">The event.</param>
+        /// <remarks>The execution is done in the publisher's thread (usually the Unity main thread).</remarks>
+        void Handle(TEvent value);
+    }
+}
