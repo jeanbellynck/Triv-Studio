461e5af841fc55bb225736efd86643974527b525
agaa-aaga <agatakim98@gmail.com>
2023-06-27 18:20:09 +0200

    Added support for generating segments of variable length

--- Assets/Code/Scripts/Levels/SegmentDescriptor.cs
+++ Assets/Code/Scripts/Levels/SegmentDescriptor.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
 
+/// <summary>
+/// Describes a level segment.
+/// </summary>
 public sealed class SegmentDescriptor : MonoBehaviour
 {
     [SerializeField]
     private float _segmentWidth;
+    
+    /// <summary>
+    /// The width of the segment.
+    /// </summary>
+    public float SegmentWidth => _segmentWidth;
 }
