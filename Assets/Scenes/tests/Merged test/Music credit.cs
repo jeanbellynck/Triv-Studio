6e04819fa829c5ec34212775c2f78ce67b0aa37f
NoePenn <noepenn1234@gmail.com>
2023-07-19 20:52:47 +0200

    sfx

---
+++ Assets/Scenes/tests/Merged test/Music credit.cs
@@ -0,0 +1,9 @@
+Cyberpunk Gaming Electro | STARDUST by Alex-Productions | https://onsound.eu/
+Music promoted by https://www.free-stock-music.com
+Creative Commons / Attribution 3.0 Unported License (CC BY 3.0)
+https://creativecommons.org/licenses/by/3.0/deed.en_US
+
+Cyberpunk Midtempo Electro Gaming Music | Future by Alex-Productions | https://onsound.eu/
+Music promoted by https://www.free-stock-music.com
+Creative Commons / Attribution 3.0 Unported License (CC BY 3.0)
+https://creativecommons.org/licenses/by/3.0/deed.en_US
